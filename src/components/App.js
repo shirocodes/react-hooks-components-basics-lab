0a10f7abdda5eb961307e265979d5aad2fd4e7e9
shirocodes <ishirapt@gmail.com>
2025-04-10 18:22:02 +0300

    Completed the lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,7 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-function About () {
-  return (
-      <div id="about">
-          <h2>About</h2>
-      </div>
-  )
-}
-
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
